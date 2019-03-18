7d036232c623f70dd002ec1f5f35ba767b9495e0
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-18 20:05:26 +0000

    Update InteractiveComponent to support activeness.

--- jw-paint-tool/core/ui/component/InteractiveComponent.h
+++ jw-paint-tool/core/ui/component/InteractiveComponent.h
@@ -38,11 +38,6 @@ namespace paint_tool {
 		virtual void onMouseMove(const POINT &mouse);
 
 		//
-		// Sets draggable to the value given
-		//
-		inline void setDraggable(const bool &_draggable);
-
-		//
 		// Sets focused to true
 		//
 		inline void giveFocus();
@@ -51,6 +46,21 @@ namespace paint_tool {
 		// Sets focused to false
 		//
 		inline void takeFocus();
+
+		//
+		// Sets active to true
+		//
+		inline void makeActive();
+
+		//
+		// Sets active to false
+		//
+		inline void makeInactive();
+
+		//
+		// Sets draggable to the value given
+		//
+		inline void setDraggable(const bool &_draggable);
 
 		//
 		// Returns true; the InteractiveComponent is ... interactive (wow)
@@ -90,6 +100,12 @@ namespace paint_tool {
 		bool focused;
 
 		//
+		// True when the Component is the last component to be clicked during
+		// the time whereby the left mouse button is held down
+		//
+		bool active;
+
+		//
 		// When true, the Component will be moved around when the user
 		// clicks-and-drags on the Component
 		//
@@ -107,6 +123,14 @@ void paint_tool::InteractiveComponent::giveFocus() {
 
 void paint_tool::InteractiveComponent::takeFocus() {
 	focused = false;
+}
+
+void paint_tool::InteractiveComponent::makeActive() {
+	active = true;
+}
+
+void paint_tool::InteractiveComponent::makeInactive() {
+	active = false;
 }
 
 bool paint_tool::InteractiveComponent::isInteractive() const {
