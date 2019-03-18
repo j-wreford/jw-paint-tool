734e9632645b84ed7447663d7156c4aee4692efa
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-18 23:12:33 +0000

    Update Window to have the component walker take in non-const Component pointers.

--- jw-paint-tool/core/ui/Window.cpp
+++ jw-paint-tool/core/ui/Window.cpp
@@ -40,7 +40,7 @@ void paint_tool::Window::onDraw() {
 
 	clrscr(0x000000);
 	
-	Window::constComponentWalker(
+	Window::componentWalker(
 		root_component.get(),
 		std::bind(&Window::drawSingleComponent, this, std::placeholders::_1)
 	);
@@ -221,9 +221,9 @@ void paint_tool::Window::drawDebugComponentPositionLines(const Component *compon
 	drawLine(rect.right + 1, rect.top + half_h, parent_rect.right, rect.top + half_h);
 }
 
-void paint_tool::Window::constComponentWalker(
-	const Component *component,
-	std::function<void(const Component *component)> fn
+void paint_tool::Window::componentWalker(
+	Component *component,
+	std::function<void(Component *component)> fn
 ) {
 
 	/* execute the function given with the currently visited component
@@ -239,7 +239,7 @@ void paint_tool::Window::constComponentWalker(
 			dynamic_cast<const ComponentGroup *>(component);
 
 		for (auto &pair : *group->getChildComponents())
-			Window::constComponentWalker(pair.second.get(), fn);
+			Window::componentWalker(pair.second.get(), fn);
 	}
 
 }
