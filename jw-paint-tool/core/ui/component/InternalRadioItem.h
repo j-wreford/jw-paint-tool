1a23dd3acbcd6f0e64dde0d4f0153ba3285af32f
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-21 22:44:08 +0000

    Update InternalRadioItem and RadioGroup to have their method definitions all inline (to fix unresolved external symbol errors).

--- jw-paint-tool/core/ui/component/InternalRadioItem.h
+++ jw-paint-tool/core/ui/component/InternalRadioItem.h
@@ -13,12 +13,18 @@
 
 namespace paint_tool {
 
+	//
+	// The string used to represent the Component
+	//
+	const std::string CPMNT_INTERNAL_RADIO_ITEM = "interal_radio_item";
+
+
 	template <class ValT>
 	class InternalRadioItem :
 		public InteractiveComponent {
 	public:
 
-		InternalRadioItem(
+		inline InternalRadioItem(
 			const	std::string	&id,
 			const	POINT		&position,
 			const	SIZE		&size,
@@ -26,17 +32,22 @@ namespace paint_tool {
 			const	std::string &style_set_id = "default"
 		);
 
-		~InternalRadioItem();
+		inline ~InternalRadioItem();
 
 		//
 		// Draws the radio item
 		//
-		void drawComponent(EasyGraphics *ctx);
+		inline virtual void drawComponent(EasyGraphics *ctx) const override;
+
+		//
+		// Returns CPMNT_INTERNAL_RADIO_ITEM
+		//
+		inline virtual std::string getComponentType() const override;
 
 		//
 		// Returns the value held by the InternalRadioItem
 		//
-		ValT getValue() const;
+		inline ValT getValue() const;
 
 	private:
 
@@ -45,4 +56,53 @@ namespace paint_tool {
 		//
 		ValT value;
 	};
+}
+
+template <class ValT>
+paint_tool::InternalRadioItem<ValT>::InternalRadioItem(
+	const	std::string	&id,
+	const	POINT		&position,
+	const	SIZE		&size,
+	ValT		value,
+	const	std::string &style_set_id
+) :
+	InteractiveComponent(id, position, size, style_set_id),
+	value(value) {
+	//
+}
+
+template <class ValT>
+paint_tool::InternalRadioItem<ValT>::~InternalRadioItem() {
+	//
+}
+
+template <class ValT>
+void paint_tool::InternalRadioItem<ValT>::drawComponent(EasyGraphics *ctx) const {
+
+	POINT pos = getAbsolutePosition();
+
+	ctx->setPenColour(0xffffff, 2);
+	ctx->selectBackColour(0xffffff);
+
+	POINT center = {
+		pos.x + (getSize().cx / 2),
+		pos.y + (getSize().cy / 2)
+	};
+
+	ctx->drawCircle(center.x, center.y, (getSize().cx / 2), false);
+
+	ComponentGroup *group = dynamic_cast<ComponentGroup *>(getParent());
+
+	if (group->getLastActiveComponent() == this)
+			ctx->drawCircle(center.x, center.y, (getSize().cx / 6), true);
+}
+
+template <class ValT>
+std::string paint_tool::InternalRadioItem<ValT>::getComponentType() const {
+	return CPMNT_INTERNAL_RADIO_ITEM;
+}
+
+template <class ValT>
+ValT paint_tool::InternalRadioItem<ValT>::getValue() const {
+	return value;
 }
--- jw-paint-tool/core/ui/component/RadioGroup.h
+++ jw-paint-tool/core/ui/component/RadioGroup.h
@@ -16,10 +16,10 @@ namespace paint_tool {
 		class ItemComponentT = InternalRadioItem<ValT>
 	>
 	class RadioGroup :
-		protected ComponentGroup {
+		public ComponentGroup {
 	public:
 
-		RadioGroup(
+		inline RadioGroup(
 			const	std::string	&id,
 			const	POINT		&position,
 			const	SIZE		&item_size,
@@ -27,18 +27,18 @@ namespace paint_tool {
 			const	std::string &style_set_id = "default"
 		);
 
-		~RadioGroup();
+		inline ~RadioGroup();
 
 		//
 		// Adds a choice to the RadioGroup
 		//
-		void addChoice(const std::string &id, ValT value);
+		inline void addChoice(ValT value);
 
 		//
 		// Returns the value of the last clicked InternalRadioItem* within
 		// the RadioGroup
 		//
-		ValT getValue() const;
+		inline ValT getValue() const;
 
 	private:
 
@@ -53,4 +53,71 @@ namespace paint_tool {
 		//
 		bool stack_vertically;
 	};
+}
+
+template <class ValT, class ItemComponentT>
+paint_tool::RadioGroup<ValT, ItemComponentT>::RadioGroup(
+	const	std::string	&id,
+	const	POINT		&position,
+	const	SIZE		&item_size,
+	const   bool		&stack_vertically,
+	const	std::string &style_set_id
+) :
+	ComponentGroup(id, position, style_set_id),
+	item_size(item_size),
+	stack_vertically(stack_vertically) {
+	//
+}
+
+template <class ValT, class ItemComponentT>
+paint_tool::RadioGroup<ValT, ItemComponentT>::~RadioGroup() {
+	//
+}
+
+template <class ValT, class ItemComponentT>
+void paint_tool::RadioGroup<ValT, ItemComponentT>::addChoice(ValT value) {
+
+	auto items = getChildComponents();
+
+	/* build the id */
+
+	std::string id = getId() + "_choice_" + std::to_string(items->size());
+
+	/* build the position */
+
+	POINT pos = { 0,0 };
+
+	if (stack_vertically)
+		pos.y = item_size.cy * items->size();
+	else
+		pos.x = item_size.cx * items->size();
+
+	p_component_t choice = std::make_unique<ItemComponentT>(
+		id,
+		pos,
+		item_size,
+		value,
+		getStyleSetId()
+		);
+
+	addComponent(choice);
+}
+
+template <class ValT, class ItemComponentT>
+ValT paint_tool::RadioGroup<ValT, ItemComponentT>::getValue() const {
+
+	ValT value;
+
+	InternalRadioItem *component = getLastActiveComponent();
+
+	if (component) {
+
+		InternalRadioItem<ValT> *choice =
+			dynamic_cast<InternalRadioItem<ValT> *>(component);
+
+		if (choice)
+			value = choice->getValue();
+	}
+
+	return value;
 }
