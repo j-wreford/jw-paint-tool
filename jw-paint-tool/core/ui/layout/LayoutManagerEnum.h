a336f41b8b29e2fc870e6676ccf8df1d62b50d94
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-22 01:33:50 +0000

    Update LayoutManagerEnum to add manual options.

--- jw-paint-tool/core/ui/layout/LayoutManagerEnum.h
+++ jw-paint-tool/core/ui/layout/LayoutManagerEnum.h
@@ -6,14 +6,17 @@ namespace paint_tool {
 	// Determines the layout mode
 	//
 	enum LayoutStrategy {
+		LAYOUT_MANUAL,
 		LAYOUT_VERTICAL,
 		LAYOUT_HORIZONTAL
 	};
 
 	//
-	// Determines how Components within a layout mode are aligned
+	// Determines how Components within a layout mode are aligned across the
+	// opposite axis
 	//
 	enum AlignStrategy {
+		ALIGN_MANUAL,
 		ALIGN_LEFT_OR_TOP,
 		ALIGN_MIDDLE,
 		ALIGN_RIGHT_OR_BOTTOM
