1c6f7b2eca1b8b46668a8681f32e3a010f94e238
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-22 01:32:05 +0000

    Add LayoutManager.cpp.

---
+++ jw-paint-tool/core/ui/layout/LayoutManager.cpp
@@ -0,0 +1,15 @@
+#include "LayoutManager.h"
+
+paint_tool::LayoutManager::LayoutManager() {
+	//
+}
+
+paint_tool::LayoutManager::~LayoutManager() {
+	//
+}
+
+void paint_tool::LayoutManager::layout(Component *component) {
+	
+	//
+
+}
