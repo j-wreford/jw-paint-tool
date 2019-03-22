f03c86060ac43be33c48557ec09686c4d01eabbb
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-22 19:16:45 +0000

    Add RadioItem.

--- jw-paint-tool/core/ui/component/RadioItem.h
+++ jw-paint-tool/core/ui/component/RadioItem.h
@@ -1,10 +1,11 @@
 #pragma once
 
-
 //
+// RadioItem
 //
+// A component to go with a RadioGroup.
 //
-//
+// This radio item component is a Label which is underlined when selected.
 //
 
 namespace paint_tool {
