927b2d4715ec6a5655fe0d5ba3468192d5c3ef9b
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-22 14:23:53 +0000

    Update RadioGroup to remove stack_vertically. The LayoutManager handles choice positioning now.

--- jw-paint-tool/core/ui/component/RadioGroup.h
+++ jw-paint-tool/core/ui/component/RadioGroup.h
@@ -23,7 +23,6 @@ namespace paint_tool {
 			const	std::string	&id,
 			const	POINT		&position,
 			const	SIZE		&item_size,
-			const   bool		&stack_vertically = true,
 			const	std::string &style_set_id = "default"
 		);
 
@@ -60,12 +59,10 @@ paint_tool::RadioGroup<ValT, ItemComponentT>::RadioGroup(
 	const	std::string	&id,
 	const	POINT		&position,
 	const	SIZE		&item_size,
-	const   bool		&stack_vertically,
 	const	std::string &style_set_id
 ) :
 	ComponentGroup(id, position, style_set_id),
-	item_size(item_size),
-	stack_vertically(stack_vertically) {
+	item_size(item_size) {
 	//
 }
 
@@ -83,18 +80,9 @@ void paint_tool::RadioGroup<ValT, ItemComponentT>::addChoice(ValT value) {
 
 	std::string id = getId() + "_choice_" + std::to_string(items->size());
 
-	/* build the position */
-
-	POINT pos = { 0,0 };
-
-	if (stack_vertically)
-		pos.y = item_size.cy * items->size();
-	else
-		pos.x = item_size.cx * items->size();
-
 	p_component_t choice = std::make_unique<ItemComponentT>(
 		id,
-		pos,
+		POINT{ 0, 0 },
 		item_size,
 		value,
 		getStyleSetId()
