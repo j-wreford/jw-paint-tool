293011ae742d6a0ba4ed5a3aace1311c50c62f3d
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-22 01:24:44 +0000

    Update LayoutManager to begin detailing how the manager will function.

--- jw-paint-tool/core/ui/layout/LayoutManager.h
+++ jw-paint-tool/core/ui/layout/LayoutManager.h
@@ -2,6 +2,38 @@
 
 #include "core\ui\component\Component.h"
 
+//
+// LayoutManager
+//
+// This class is responsible for positioning Components.
+//
+// Each ComponentGroup has a layout_style property. This property determines
+// which strategy to use when the layout manager positions components.
+// Only two strategies are available: LAYOUT_VERTICAL and LAYOUT_HORIZONTAL.
+//
+// The LayoutManager has no concept of wrapping to a new line.
+//
+// Each Component has a position_style property. This property determines how
+// a Component is positioned on the oppose axis. For example, when using the
+// LAYOUT_VERTICAL layout_style, the position_style property will determine
+// its horizontal position (left, middle, or right).
+//
+
+namespace paint_tool {
+
+	class LayoutManager {
+	public:
+
+		LayoutManager();
+		~LayoutManager();
+
+		//
+		// Begins the layout process
+		//
+		void layout(Component *component);
+	};
+}
+
 /*
 
 in Window::onDraw()...
