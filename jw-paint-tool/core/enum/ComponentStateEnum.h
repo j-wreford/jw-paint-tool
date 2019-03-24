6e204984d24fb3715771dc2cd9b1b453f5e4a0f0
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-24 19:54:39 +0000

    Add ComponentStateEnum.

---
+++ jw-paint-tool/core/enum/ComponentStateEnum.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace paint_tool {
+
+	//
+	// Describes the state of a Component
+	//
+	enum ComponentState {
+		COMPONENT_STATE_NORMAL,
+		COMPONENT_STATE_ACTIVE,
+		COMPONENT_STATE_FOCUSED,
+		COMPONENT_STATE_HOVERED
+	};
+}
