b98bd9117d20da2bbd0b77af592e0706ac07b012
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-25 18:29:55 +0000

    Update ChoiceComponent to add value to the constructor paramter.

--- jw-paint-tool/core/ui/component/ChoiceComponent.h
+++ jw-paint-tool/core/ui/component/ChoiceComponent.h
@@ -9,7 +9,7 @@ namespace paint_tool {
 		public ValueComponent<T> {
 	public:
 
-		ChoiceComponent(const std::string &id);
+		ChoiceComponent(const std::string &id, T value);
 		~ChoiceComponent();
 
 		//
@@ -33,8 +33,8 @@ namespace paint_tool {
 }
 
 template <typename T>
-paint_tool::ChoiceComponent<T>::ChoiceComponent(const std::string &id) :
-	ValueComponent<T>(id),
+paint_tool::ChoiceComponent<T>::ChoiceComponent(const std::string &id, T value) :
+	ValueComponent<T>(id, value),
 	chosen(false) {
 	//
 }
