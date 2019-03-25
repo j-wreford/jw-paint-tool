695e2becce0f97f730d71a675fa34d5afdd166e6
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-25 16:05:16 +0000

    Add ToolEnum.

---
+++ jw-paint-tool/src/enum/ToolEnum.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace paint_tool {
+
+	//
+	// Describes a tool selection
+	//
+	enum Tool {
+		TOOL_MOVE,
+		TOOL_DEL,
+		TOOL_PEN_FREEHAND,
+		TOOL_PEN_LINE,
+		TOOL_SHAPE_TRI,
+		TOOL_SHAPE_RECT,
+		TOOL_SHAPE_CIRLCE,
+		TOOL_SHAPE_STAR
+	};
+}
