726d1b60a8731862a70a873320e6dd5155bdd9b9
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-25 16:59:11 +0000

    Add ChoiceComponent.

---
+++ jw-paint-tool/core/ui/component/ChoiceComponent.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "core\ui\component\ValueComponent.h"
+
+namespace paint_tool {
+
+	template <typename T>
+	class ChoiceComponent :
+		public ValueComponent<T> {
+	public:
+
+		ChoiceComponent(const std::string &id);
+		~ChoiceComponent();
+
+		//
+		// Returns the chosen property
+		//
+		inline bool isChosen() const;
+
+		//
+		// Sets the chosen property
+		//
+		inline void setChosen(const bool &_chosen);
+
+	private:
+
+		//
+		// True when the ChoiceComponent was the last one clicked inside its
+		// containing ChoiceGroup
+		//
+		bool chosen;
+	};
+}
+
+template <typename T>
+paint_tool::ChoiceComponent<T>::ChoiceComponent(const std::string &id) :
+	ValueComponent<T>(id) {
+	//
+}
+
+template <typename T>
+paint_tool::ChoiceComponent<T>::~ChoiceComponent() {
+	//
+}
+
+template <typename T>
+bool paint_tool::ChoiceComponent<T>::isChosen() const {
+	return chosen;
+}
+
+template <typename T>
+void paint_tool::ChoiceComponent<T>::setChosen(const bool &_chosen) {
+	chosen = _chosen;
+}
