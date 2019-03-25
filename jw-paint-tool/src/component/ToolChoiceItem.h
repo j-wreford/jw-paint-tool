a2bd1e89ab9d3123146a06f56f91d3adaa8ad3ca
Joe Wreford <Joe.Wreford@outlook.com>
2019-03-25 16:05:30 +0000

    Add ToolChoiceItem.h.

---
+++ jw-paint-tool/src/component/ToolChoiceItem.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "core\ui\component\RadioItem.h"
+
+//
+// ToolChoiceItem
+//
+// A tailored RadioItem for the tools choice RadioGroup UI component.
+//
+//
+
+namespace paint_tool {
+
+	class ToolChoiceItem {
+
+	};
+}
