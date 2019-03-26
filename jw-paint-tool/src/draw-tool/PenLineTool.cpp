f5f5968a4277e63c3b579ea73614ff014e63788a
Joe Wreford <joe.wreford@outlook.com>
2019-03-26 15:37:31 +0000

    Update PenLineTool to add drawing logic.

--- jw-paint-tool/src/draw-tool/PenLineTool.cpp
+++ jw-paint-tool/src/draw-tool/PenLineTool.cpp
@@ -11,6 +11,8 @@ paint_tool::PenLineTool::~PenLineTool() {
 
 void paint_tool::PenLineTool::draw(const POINT & mouse) {
 
-	
+	Drawing *drawing = getDrawing();
 
+	*drawing = getInitialMouse();
+	*drawing += mouse;
 }
