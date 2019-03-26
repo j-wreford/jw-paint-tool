676513e96c1a112b2c5cd75a2f127b5d6c984017
Joe Wreford <joe.wreford@outlook.com>
2019-03-26 15:12:48 +0000

    Add PenLineTool.

---
+++ jw-paint-tool/src/draw-tool/PenLineTool.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "src\draw-tool\IDrawTool.h"
+
+namespace paint_tool {
+
+	class PenLineTool :
+		public IDrawTool {
+	public:
+
+		PenLineTool(Drawing *drawing, const POINT &mouse);
+		~PenLineTool();
+
+		//
+		// Sets two points, the first starting on the location of the
+		// initial mouse down press, and the last on the current mouse move
+		// point
+		//
+		virtual void draw(const POINT & mouse);
+	};
+}
