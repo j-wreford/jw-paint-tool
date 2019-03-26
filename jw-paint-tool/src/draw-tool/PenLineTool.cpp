bce3459ba13363479429e8b909d5096bd0bd1652
Joe Wreford <joe.wreford@outlook.com>
2019-03-26 15:12:48 +0000

    Add PenLineTool.

---
+++ jw-paint-tool/src/draw-tool/PenLineTool.cpp
@@ -0,0 +1,16 @@
+#include "PenLineTool.h"
+
+paint_tool::PenLineTool::PenLineTool(Drawing *drawing, const POINT &mouse) :
+	IDrawTool(drawing, mouse) {
+	//
+}
+
+paint_tool::PenLineTool::~PenLineTool() {
+	//
+}
+
+void paint_tool::PenLineTool::draw(const POINT & mouse) {
+
+	
+
+}
