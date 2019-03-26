80ad47f53f5c52c981f9f8fb545ca77eb7e4326e
Joe Wreford <joe.wreford@outlook.com>
2019-03-26 00:06:09 +0000

    Update Canvas to optimise the background drawing (now draws half the amount of rectanges).

--- jw-paint-tool/src/component/Canvas.cpp
+++ jw-paint-tool/src/component/Canvas.cpp
@@ -18,8 +18,9 @@ void paint_tool::Canvas::drawComponent(EasyGraphics *ctx) const {
 	RECT rect = getAbsoluteRect();
 
 	const int square = 15;
-	const int even_col = 0xffffff;
-	const int odd_col = 0xf7f7f7;
+	const int col = 0xf7f7f7;
+	ctx->selectBackColour(col);
+	ctx->setPenColour(col, 1);
 
 	for (int i = rect.top; i < rect.bottom; i += square) {
 
@@ -28,11 +29,8 @@ void paint_tool::Canvas::drawComponent(EasyGraphics *ctx) const {
 			/* just for this canvas component, ignore the whole styleset system
 			   and set the colours here */
 
-			int colour = ((j + i) % 2) == 1 ? even_col : odd_col;
-			ctx->selectBackColour(colour);
-			ctx->setPenColour(colour, 1);
-
-			ctx->drawRectangle(j, i, square, square, true);
+			if (((j + i) % 2) == 1)
+				ctx->drawRectangle(j, i, square, square, true);
 		}
 	}
 }
