342ade73a636ced7c3f53d9eee889d08af47319b
Joe Wreford <joe.wreford@outlook.com>
2019-03-26 16:17:17 +0000

    Update Drawing to make points relative to where the Canvas is on the screen.

--- jw-paint-tool/src/component/Drawing.cpp
+++ jw-paint-tool/src/component/Drawing.cpp
@@ -13,23 +13,36 @@ paint_tool::Drawing::~Drawing() {
 
 void paint_tool::Drawing::drawComponent(EasyGraphics *ctx) const {
 
+	// While testing, to make it obvious whats being drawn
+	ctx->setPenColour(EasyGraphics::clDarkMagenta, 2);
+
 	if (points.size() > 0) {
 
-		POINT *last = points.front();
+		POINT pos = getAbsolutePosition();
+
+		POINT last = {
+			points.front()->x += pos.x,
+			points.front()->y += pos.y
+		};
 
 		auto start_position = points.begin();
 		std::advance(start_position, 1);
 
-		std::for_each(start_position, points.end(), [&last, &ctx](POINT *point) {
+		std::for_each(start_position, points.end(), [&pos, &last, &ctx](POINT *point) {
 
-			ctx->drawLine(last->x, last->y, point->x, point->y);
-			last = point;
+			POINT next = {
+				point->x += pos.x,
+				point->y += pos.y
+			};
+
+			ctx->drawLine(last.x, last.y, next.x, next.y);
+			last = next;
 		});
 	}
 }
 
 void paint_tool::Drawing::recalculateSize() {
-
+	/*
 	setSize(SIZE{ 0,0 });
 	RECT rect = getRect();
 
@@ -46,7 +59,7 @@ void paint_tool::Drawing::recalculateSize() {
 			rect.bottom = point->y;
 	}
 
-	/* need to update the nwe origin etc etc */
+	/* need to update the nwe origin etc etc
 
-	setRect(rect);
+	setRect(rect);*/
 }
