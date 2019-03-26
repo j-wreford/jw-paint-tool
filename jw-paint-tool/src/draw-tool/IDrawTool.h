27cee64b30d6c26cb884141ea73e73a1d8e2de3e
Joe Wreford <joe.wreford@outlook.com>
2019-03-26 15:11:42 +0000

    Update IDrawTool to add the initial location of the mouse pointer when drawing began.

--- jw-paint-tool/src/draw-tool/IDrawTool.h
+++ jw-paint-tool/src/draw-tool/IDrawTool.h
@@ -15,13 +15,20 @@ namespace paint_tool {
 	class IDrawTool {
 	protected:
 
-		inline IDrawTool(Drawing *drawing);
+		inline IDrawTool(Drawing *drawing, const POINT &mouse);
 		inline ~IDrawTool();
 
 		//
 		// Manipulates points of the Drawing component
 		//
-		virtual void draw(const POINT & mouse) = 0;
+		virtual void draw(const POINT &mouse) = 0;
+
+	protected:
+
+		//
+		// Returns the inital mouse down POINT
+		//
+		inline POINT getInitialMouse() const;
 
 	private:
 
@@ -29,14 +36,24 @@ namespace paint_tool {
 		// The Drawing component to add point data to
 		//
 		Drawing *drawing;
+
+		//
+		// The POINT of the initial left mouse down
+		//
+		const POINT initial_mouse;
 	};
 }
 
-paint_tool::IDrawTool::IDrawTool(Drawing *drawing) :
-	drawing(drawing) {
+paint_tool::IDrawTool::IDrawTool(Drawing *drawing, const POINT &mouse) :
+	drawing(drawing),
+	initial_mouse(mouse){
 	//
 }
 
 paint_tool::IDrawTool::~IDrawTool() {
 	//
+}
+
+POINT paint_tool::IDrawTool::getInitialMouse() const {
+	return initial_mouse;
 }
