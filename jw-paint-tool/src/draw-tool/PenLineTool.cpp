51c9dca25f0e166016f5d3a9d40e0481bb8a7751
Joe Wreford <joe.wreford@outlook.com>
2019-03-26 21:05:20 +0000

    Update PenLineTool to add points relative to the Drawing component, and to instruct the Drawing Component to recalculate its size after changing its points list.

--- jw-paint-tool/src/draw-tool/PenLineTool.cpp
+++ jw-paint-tool/src/draw-tool/PenLineTool.cpp
@@ -13,6 +13,13 @@ void paint_tool::PenLineTool::draw(const POINT & mouse) {
 
 	Drawing *drawing = getDrawing();
 
-	*drawing = getInitialMouse();
-	*drawing += mouse;
+	POINT relative_mouse = {
+		mouse.x - drawing->getPosition().x,
+		mouse.y - drawing->getPosition().y
+	};
+
+	*drawing = POINT{ 0, 0 };
+	*drawing += relative_mouse;
+
+	drawing->recalculateSize();
 }
