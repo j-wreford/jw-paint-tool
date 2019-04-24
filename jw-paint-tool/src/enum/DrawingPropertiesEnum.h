d32a36f0f6fb117370503b37b15cd5437722d01d
Joe Wreford <Joe.Wreford@outlook.com>
2019-04-24 17:48:36 +0100

    Update DrawingPropertiesEnum.

--- jw-paint-tool/src/enum/DrawingPropertiesEnum.h
+++ jw-paint-tool/src/enum/DrawingPropertiesEnum.h
@@ -6,10 +6,10 @@ namespace paint_tool {
 	// Describes properties a drawing can have
 	//
 	enum DrawingProperties {
+		DRAW_PROP_MOVE,
+		DRAW_PROP_DEL,
 		DRAW_PROP_COL_FILL,
 		DRAW_PROP_COL_LINE,
-		DRAW_PROP_LINE_THICKNESS,
-		DRAW_PROP_MOVE,
-		DRAW_PROP_DEL
+		DRAW_PROP_LINE_THICKNESS
 	};
 }
