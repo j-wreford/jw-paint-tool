0de054c002ca37ef0bf8a7e69b98e7ec83206ac1
Joe Wreford <Joe.Wreford@outlook.com>
2019-04-28 13:02:22 +0100

    Add ColourChoiceItem.

---
+++ jw-paint-tool/src/component/ColourChoiceItem.cpp
@@ -0,0 +1,19 @@
+#include "ColourChoiceItem.h"
+
+paint_tool::ColourChoiceItem::ColourChoiceItem(
+	const	std::string	&id,
+	const	SIZE		&size,
+	const	int			&value
+) : ChoiceComponent<int>(id, value) {
+
+	p_component_t box = std::make_unique<StaticBox>(
+		id + "_box", size
+	);
+	box->setBgColour(value);
+
+	addComponent(box);
+}
+
+paint_tool::ColourChoiceItem::~ColourChoiceItem() {
+	//
+}
