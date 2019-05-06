54f2c99b3b567aea6951399fb60cada9b8f4cb56
Joe Wreford <Joe.Wreford@outlook.com>
2019-05-06 21:37:49 +0100

    Update PaintToolFileIO to add comments to illustrate understanding of sample code.

--- jw-paint-tool/src/PaintToolFileIO.cpp
+++ jw-paint-tool/src/PaintToolFileIO.cpp
@@ -14,68 +14,111 @@ const COMDLG_FILTERSPEC paint_tool::PaintToolFileIO::FILE_TYPES[] = {
 
 void paint_tool::PaintToolFileIO::open() {
 
-	// CoCreate the File Open Dialog object.
-	IFileDialog *pfd = NULL;
-	HRESULT hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pfd));
-	if (SUCCEEDED(hr))
-	{
-			// Set the options on the dialog.
-			DWORD dwFlags;
+	/* show the open dialog in order to know which file we're going to
+	   process */
 
-			// Before setting, always get the options first in order not to override existing options.
-			hr = pfd->GetOptions(&dwFlags);
-			if (SUCCEEDED(hr))
-			{
-				// In this case, get shell items only for file system items.
-				hr = pfd->SetOptions(dwFlags | FOS_FORCEFILESYSTEM);
-				if (SUCCEEDED(hr))
-				{
-					// Set the file types to display only. Notice that, this is a 1-based array.
-					hr = pfd->SetFileTypes(ARRAYSIZE(FILE_TYPES), FILE_TYPES);
-					if (SUCCEEDED(hr))
-					{
-						// Set the selected file type index to Word Docs for this example.
-						hr = pfd->SetFileTypeIndex(1);
-						if (SUCCEEDED(hr))
-						{
-							// Set the default extension to be ".doc" file.
-							hr = pfd->SetDefaultExtension(L"jwpt");
-							if (SUCCEEDED(hr))
-							{
-								// Show the dialog
-								hr = pfd->Show(NULL);
-								if (SUCCEEDED(hr))
-								{
-									// Obtain the result, once the user clicks the 'Open' button.
-									// The result is an IShellItem object.
-									IShellItem *psiResult;
-									hr = pfd->GetResult(&psiResult);
+	HRESULT hr = showOpenDialog();
+
+}
+
+HRESULT paint_tool::PaintToolFileIO::showOpenDialog() {
+
+	/* initialise the property which will be used to control the open dialog
+	   with */
+
+	IFileDialog *pfd = NULL;
+
+	/* create the file open dialog using CoCreateInstance. from looking at docs,
+	   this function is used to instantiate only one object. so perhaps, a
+	   singleton-like pattern? not too sure */
+
+	HRESULT hr = ::CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pfd));
+	
+	if (SUCCEEDED(hr)) {
+
+		/* set the options on the dialog. this seems like boilerplate code to
+		   show files that are on the file system */
+
+		DWORD dwFlags;
+		hr = pfd->GetOptions(&dwFlags);
+
+		if (SUCCEEDED(hr)) {
+
+			hr = pfd->SetOptions(dwFlags | FOS_FORCEFILESYSTEM);
+
+			if (SUCCEEDED(hr)) {
+
+				/* inform the dialog that we're only interested in files that the
+				   FILE_TYPES static property holds. this will only ever be
+				   .jwpt files for this application */
+
+				hr = pfd->SetFileTypes(ARRAYSIZE(FILE_TYPES), FILE_TYPES);
+
+				if (SUCCEEDED(hr)) {
+
+					/* the file types index begins at 1, and we only have one
+					   entry within it. call SetFilTypeIndex to set it to our
+					   custom file format (.jwpt) */
+
+					hr = pfd->SetFileTypeIndex(1);
+
+					if (SUCCEEDED(hr)) {
+
+						hr = pfd->SetDefaultExtension(L"jwpt");
+
+						if (SUCCEEDED(hr)) {
+
+							/* show the open dialog! */
+
+							hr = pfd->Show(NULL);
+							
+							if (SUCCEEDED(hr)) {
+								
+								/* as a guess, this body of code is only reached
+								   once the open file dialog exits somehow
+								   (either through the cross button, cancel, or
+								   hopefully the open button) */
+
+								/* attempt to fetch the result of the open file
+								   dialog */
+								
+								IShellItem *psiResult;
+								hr = pfd->GetResult(&psiResult);
+
+								if (SUCCEEDED(hr)) {
+
+									/* there was a result, and we now have
+									   access to the information we need to 
+									   begin processing the selected .jwpt 
+									   file */
+
+									/*PWSTR pszFilePath = NULL;
+									hr = psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
 									if (SUCCEEDED(hr))
 									{
-										// We are just going to print out the name of the file for sample sake.
-										/*PWSTR pszFilePath = NULL;
-										hr = psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-										if (SUCCEEDED(hr))
-										{
-											TaskDialog(NULL,
-												NULL,
-												L"CommonFileDialogApp",
-												pszFilePath,
-												NULL,
-												TDCBF_OK_BUTTON,
-												TD_INFORMATION_ICON,
-												NULL);
-											CoTaskMemFree(pszFilePath);
-										}
-										*/
-										psiResult->Release();
+										TaskDialog(NULL,
+											NULL,
+											L"CommonFileDialogApp",
+											pszFilePath,
+											NULL,
+											TDCBF_OK_BUTTON,
+											TD_INFORMATION_ICON,
+											NULL);
+										CoTaskMemFree(pszFilePath);
 									}
+									*/
+
+									psiResult->Release();
 								}
 							}
 						}
 					}
 				}
+			}
 		}
+
 		pfd->Release();
 	}
+
+	return hr;
 }
--- jw-paint-tool/src/PaintToolFileIO.h
+++ jw-paint-tool/src/PaintToolFileIO.h
@@ -8,8 +8,15 @@
 //
 // PaintToolFileIO
 //
-// Provides file input and output using Windows' Common File Dialog API in order
+// Provides file input and output using Windows' Common Item Dialog API in order
 // to save and load .jwpt files within the application.
+//
+// Some code here has been adapted from Microsoft's Common Item Dialog
+// documentation page. The resource used can be found at the following URL:
+// https://msdn.microsoft.com/en-us/library/Bb776913(v=VS.85).aspx#basic_usage
+// 
+// As a note, I have attempted to comment my understanding of the code I have
+// used to show that I am not blindly plagiarising the work!
 //
 
 namespace paint_tool {
@@ -35,5 +42,10 @@ namespace paint_tool {
 		// The allowed file types when opening a file
 		//
 		static const COMDLG_FILTERSPEC FILE_TYPES[];
+
+		//
+		// Performs the neccessary operations to show the open file dialog
+		//
+		HRESULT showOpenDialog();
 	};
 }
